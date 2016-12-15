189d179bacd3bc6725526912b7a35376ccff0798
AniMysore74 <AniMysore74@users.noreply.github.com>
2016-12-15 19:18:06 +0530

    Travis :knife:

--- travis_scripts/build.sh
+++
@@ -1,7 +0,0 @@
-# TODO: improve this please :P
-cp index.html public/index.html
-cp .travis.yml public/.travis.yml
-cp README.md public/README.md
-cp -R travis_scripts/ public/travis_scripts/
-cp -R assets/ public/assets/
-cp -R bower_components/ public/bower_components/
