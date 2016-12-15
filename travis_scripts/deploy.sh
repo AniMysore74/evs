db1d0c349520033f46253613d96c4f5e2ec458aa
AniMysore74 <AniMysore74@users.noreply.github.com>
2016-12-15 10:49:03 +0530

    really :angry:

--- travis_scripts/deploy.sh
+++ travis_scripts/deploy.sh
@@ -11,11 +11,19 @@ git config --global user.name "Travis CI"
 
 # build (CHANGE THIS)
 
+# TODO: improve this please :P
+cp index.html public/index.html
+cp .travis.yml public/.travis.yml
+cp README.md public/README.md
+cp -R travis_scripts/ public/travis_scripts/
+cp -R assets/ public/assets/
+cp -R bower_components/ public/bower_components/
 
 # deploy
 cd public
 
 git init
 git add --all
+git rm node_modules/
 git commit -a -m "Deploy to Github Pages"
 git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
